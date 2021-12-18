310381106fcddc28c0098da72c8f8ff69233e26b
KRISHNA CHANDHOK <52276473+krishna619@users.noreply.github.com>
2021-12-18 19:31:00 +0530

    Update average.js

--- average.js
+++ average.js
@@ -6,7 +6,8 @@ function average(numbers) {
 }
 describe('Function returning NaN', function() {
     it('shall return NaN', function() {
-        assert.notEqual(isNaN(average(number2)), true); // No AssertionError
+        number2.isNaN(NaN)
+        //assert.notEqual(isNaN, true); // No AssertionError
     });
 });
 
