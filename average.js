61e1306e9156954340549be6259159556788af59
KRISHNA CHANDHOK <52276473+krishna619@users.noreply.github.com>
2021-12-18 18:23:21 +0530

    Update average.js

--- average.js
+++ average.js
@@ -2,11 +2,7 @@ require('assert');
 function average(numbers) {
   return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
 }
-describe('Function returning NaN', average(numbers) {
-  it('shall return NaN', average(numbers) {
-    assert.equal(isNaN(functionUnderTest()), false); // AssertionError: NaN == NaN
-    assert.notEqual(isNaN(functionUnderTest()), true); // No AssertionError
-  });
+
 });
 
 module.exports = {average};
