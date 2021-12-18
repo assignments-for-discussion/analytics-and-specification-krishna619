e1a409891aa6a7d41bb3e97e722aa66fed85c68d
KRISHNA CHANDHOK <52276473+krishna619@users.noreply.github.com>
2021-12-18 18:54:02 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,15 +1,14 @@
-const assert = require("assert");
 var x=0;
 var i=0;
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / (numbers.length - x);
-}
-for(i=0;i<numbers.length;i++){
+  for(i=0;i< (numbers.length);i++){
   if(average[i]=='NaN'){
     x=x+1;
-    continue;
-  }
+    continue;}
 }
+  return numbers.reduce((p, c)=> p + c, 0) / (numbers.length - x);
+}
+
 
 
 module.exports = {average};
