83ef213c04966cf10cba796cd08737d6f8398aef
KRISHNA CHANDHOK <52276473+krishna619@users.noreply.github.com>
2021-12-18 19:22:51 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,15 +1,13 @@
 const assert = require("assert");
 
 function average(numbers) {
-    describe('Function returning NaN', function() {
-        it('shall return NaN', function() {
-            assert.notEqual(isNaN(average(numbers)), true); // No AssertionError
-        });
-    });
     return numbers.reduce((p, c) => p + c, 0) / numbers.length;
-
 }
-
+describe('Function returning NaN', function() {
+    it('shall return NaN', function() {
+        assert.notEqual(isNaN(average(numbers)), true); // No AssertionError
+    });
+});
 
 
 
