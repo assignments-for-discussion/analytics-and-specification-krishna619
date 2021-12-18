4aed00cf7c7582c850b546f2ec4ccfd786e081f0
KRISHNA CHANDHOK <52276473+krishna619@users.noreply.github.com>
2021-12-18 19:11:15 +0530

    Update average.js

--- average.js
+++ average.js
@@ -2,17 +2,18 @@ var x=0;
 var i=0;
 var sum = 0;
 function average(numbers) {
-  for(i=0;i< (numbers.length){
-     
-    if(average[i]=='NaN'){
-      x=x+1;
-      continue;}
+  for(i=0;i< (numbers.length)
+      if(average[i]=='NaN')
+      {
+        x=x+1;
+        continue;
+      }
       sum = sum + average[i];
-}
-  if( numbers.length==0 || numbers.length == x)
+
+     if( numbers.length==0 || numbers.length == x)
       {
         return NaN;
-  }
+      }
   return sum / (numbers.length - x);
 }
 
